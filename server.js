2e91153869ac1dd287dbd2e34ab45252323d7292
Ugwuanyi Annastesia Amarachi <annajoan@gmail.com>
2026-05-10 22:06:33 +0100

    fix server crash on railway

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes")
 const passwordRoutes = require('./routes/passwordRoutes')
@@ -39,6 +38,14 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`✅ Server running on port ${PORT}`);
+    });
   })
-  .catch((err) => console.log("❌ Connection error:", err));
+  .catch((err) => {
+    console.error("❌ MongoDB Connection Error:", err);
+    process.exit(1); // THIS IS IMPORTANT
+  });
